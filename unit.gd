06e7e545022fe60a9cc67b80ad6709000318db16
Yev Nosenko <yevhenii2208@gmail.com>
2026-05-27 15:02:34 -0400

    Got the colors and textures to cooperate, now I just have to make sure all my attributes also work :)

--- unit.gd
+++ unit.gd
@@ -16,9 +16,8 @@ var rng = RandomNumberGenerator.new()
 @export var armor_mbt = ["tank_division", 3.0]
 
 # complete list of states and corresponding unit color
-var STATES = [["federal", "teklasiana", "commonwealth", "cascadia", "cspc"], ["circle", "mormon", "unions"], "tinkle"]
-var UNITCOLORS = [[Color(50,60,210,255), ], [], ]
-var STATECOLORS = [[Color(37, 52, 148, 255), ], [], ]
+var STATES = [ ["federal", "teklasiana", "commonwealth", "cascadia", "cspc"] , ["circle", "mormon", "unions"] , "tinkle"]
+var UNITCOLORS = [[Color8(50,60,210,255), Color8(15,85,125,255), Color8(130,165,140,255), Color8(65,230,120,255), Color8(210,130,90,255)], [Color8(0,0,0,255), Color8(185,150,255,255), Color8(135.0, 18.004, 19.406, 1.0)], Color8(200,0,200,255)]
 
 
 # internal variables not meant for human eyes
@@ -29,17 +28,21 @@ var BASESPEED = 200.0
 var DIVISIONS = [inf_um, inf_mntrs, inf_mil, inf_strm, inf_para, inf_sf, inf_moto, inf_mech, armor_mbt]
 var division_type = []
 var allegiance = "federal"
-var unit_color = Color(0,0,0,0);
+var unit_color = Color8(0,0,0,0);
 
 func _ready() -> void:
 	randomize()
 	randomize()
 	division_type = DIVISIONS.pick_random()
-	allegiance = STATES.pick_random()
+	var luck = rng.randi_range(0, 2)
+	allegiance = STATES[luck]
+	unit_color = UNITCOLORS[luck]
 	if typeof(allegiance) == TYPE_ARRAY:
-		allegiance = allegiance.pick_random()
+		luck = rng.randi_range(0,len(allegiance) - 1)
+		allegiance = allegiance[luck]
+		unit_color = unit_color[luck]
 	
-	select_graphic(division_type[0], color)
+	select_graphic(division_type[0], unit_color)
 	
 	
 
