6d0cfcdb448629338c940d88fffe545d12178367
Yev Nosenko <yevhenii2208@gmail.com>
2026-05-29 13:31:10 -0400

    I lied. I am now reworking the unit spawn system to use a single function. Hoepfully it's not gonna be too hard.

--- testing.gd
+++ testing.gd
@@ -3,11 +3,38 @@ var unit_scene: PackedScene = preload("res://unit.tscn")
 
 func _ready():
 	var units = $units
-	var spawn_pos = $spawns/marker1
-	var unit = unit_scene.instantiate() as CharacterBody2D
-	unit.position = spawn_pos.position
-	units.add_child(unit)
-	unit.set_allegiance(0, 0)
-	unit.set_unit_type(7)
-	spawn_pos = $all_spawn/unit_spawns_circle.get_children()
+	var spawn_pos = $spawns.get_children()
+	var fed = $spawns/federal
+	var circle = $spawns/golden_circle
 	
+	for i in spawn_pos:
+		for y in i.get_children():
+			var unit = unit_scene.instantiate() as CharacterBody2D
+			units.add_child(unit)
+			if i == fed:
+				configure_unit(unit, 0, 0, 7, y.global_position)
+				#print("federal")
+			if i == circle:
+				if y == $spawns/golden_circle/marker1:
+					configure_unit(unit, 1, 0, 2, y.global_position)
+				else:
+					configure_unit(unit, 1, 0, 3, y.global_position)
+				#print("circle")
+
+#_________________________________________________________
+# Federal, Teklasiana, Commonwealth, Cascadia, Consumerist
+# 0, 0   | 0, 1      | 0, 2        | 0, 3    | 0, 4
+
+# Golden Circle, Mormon, Unions
+# 1, 0         | 1, 1  | 1, 2
+
+# Tinkle
+# 3, any value
+
+# inf_um, inf_mntrs, inf_mil, inf_strm, inf_para, inf_sf, inf_moto, inf_mech, armor_mbt
+# 0       1          2        3         4         5       6         7         8
+
+# unit, faction, state, unit type
+func configure_unit(u: CharacterBody2D, f: int, s: int, ut: int, pos) -> void:
+	if f > 2 or f < 0:
+		f == 0
