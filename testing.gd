ea6e200018cb81ae7595192f47e4d932517b5faf
Yev Nosenko <yevhenii2208@gmail.com>
2026-06-10 19:39:15 -0400

    Basic clean-up. Deleted a few old nodes and did some basic testing for the spawning system. I thought I'd have more time but nope.

--- testing.gd
+++ testing.gd
@@ -3,11 +3,18 @@ var unit_scene: PackedScene = preload("res://unit.tscn")
 
 func _ready():
 	var units = $units
-	var spawn_pos = $spawns/marker1
 	var unit = unit_scene.instantiate() as CharacterBody2D
-	unit.position = spawn_pos.position
 	units.add_child(unit)
 	unit.set_allegiance(0, 0)
 	unit.set_unit_type(7)
-	spawn_pos = $all_spawn/unit_spawns_circle.get_children()
+	var spawn_pos = $spawns.get_children()
+	unit.global_position = spawn_pos[0].global_position
+	units.add_child(unit)
+	unit.set_allegiance(0, 0)
+	unit.set_unit_type(7)
+	unit.global_position = spawn_pos[1].global_position
+	units.add_child(unit)
+	unit.set_allegiance(0, 0)
+	unit.set_unit_type(7)
+	unit.global_position = spawn_pos[1].global_position
 	
