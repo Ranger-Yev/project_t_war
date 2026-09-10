f8d9543001ef009f27db1f7dd16af790f14876a0
Yev Nosenko <yevhenii2208@gmail.com>
2026-09-10 12:05:43 -0400

    Began implementation of the selection box but the documentation for collision boxes is so confusing ;-; I'd say I might be around 10-20% done so far.

--- selector.gd
+++ selector.gd
@@ -6,12 +6,19 @@ func _process(_delta: float) -> void:
 
 func _draw():
 	if Input.is_action_just_pressed("LMB"):
-		start = get_viewport().get_mouse_position()
+		start = get_viewport().get_mouse_position() # starting coords of the selection box.
 	if Input.is_action_pressed("LMB"):
-		var cur_mouse_pos = get_viewport().get_mouse_position()
-		var sel_rect = Rect2(start.x, start.y, cur_mouse_pos.x - start.x, cur_mouse_pos.y - start.y)
+		var cur_mouse_pos = get_viewport().get_mouse_position() # current moujse position
+		var rect_size = Vector2(cur_mouse_pos.x - start.x, cur_mouse_pos.y - start.y) # gets proper size of the selection box
+		var sel_rect = Rect2(start.x, start.y, rect_size.x, rect_size.y) # actual selection box
 		#print("LMB")
-		draw_rect(sel_rect, Color(0.429, 0.611, 0.824, 0.549), true)
+		draw_rect(sel_rect, Color(0.429, 0.611, 0.824, 0.549), true) # drawing the actual selection box
+		touching_units(start, rect_size)
 	if Input.is_action_just_released("LMB"):
-		start = Vector2.ZERO
+		start = Vector2.ZERO # resets the starting coords
 	#print(start, " : ", get_viewport().get_mouse_position())
+
+func touching_units(start: Vector2, rect_size: Vector2): # the unit detection box :3
+	var collision_box: CollisionShape2D =  $Area2D/CollisionShape2D
+	collision_box.shape.extents = Vector2(rect_size)
+	collision_box.global_position = start
