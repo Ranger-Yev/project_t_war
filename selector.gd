2af775de34d6110d929ff4f0b342b50df652654c
Yev Nosenko <yevhenii2208@gmail.com>
2026-09-10 12:28:45 -0400

    The collision box was actually super easy to fix, nevermind all that crying and complaining in the earlier commit. Now unto actual unit selection :) Feature maybe at 40-50% complete.

--- selector.gd
+++ selector.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var start = null
+@onready var collision_box: CollisionShape2D =  $Area2D/CollisionShape2D
 
 func _process(_delta: float) -> void:
 	queue_redraw()
@@ -13,12 +14,18 @@ func _draw():
 		var sel_rect = Rect2(start.x, start.y, rect_size.x, rect_size.y) # actual selection box
 		#print("LMB")
 		draw_rect(sel_rect, Color(0.429, 0.611, 0.824, 0.549), true) # drawing the actual selection box
+		
 		touching_units(start, rect_size)
 	if Input.is_action_just_released("LMB"):
 		start = Vector2.ZERO # resets the starting coords
+		collision_box.shape.set_size(Vector2.ZERO) # resets the collision box for good luck :3
 	#print(start, " : ", get_viewport().get_mouse_position())
 
-func touching_units(start: Vector2, rect_size: Vector2): # the unit detection box :3
-	var collision_box: CollisionShape2D =  $Area2D/CollisionShape2D
-	collision_box.shape.extents = Vector2(rect_size)
-	collision_box.global_position = start
+func touching_units(start_coords: Vector2, rect_size: Vector2): # the unit detection box :3
+	start_coords = Vector2(start_coords.x + (rect_size.x / 2), start_coords.y + (rect_size.y / 2))
+	
+	collision_box.shape.set_size(abs(Vector2(rect_size)))
+	collision_box.global_position = start_coords
+	
+
+		
