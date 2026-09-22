9817614b8be690c0c53d668c935b30016e465d02
Yev Nosenko <yevhenii2208@gmail.com>
2026-09-22 07:27:11 -0400

    Moved the python script, finished the basic structure of the .ini file and commented in explanations.

--- sutfc.py
+++
@@ -1,2 +0,0 @@
-# this program is here to help create a config file without giving myself artithis. 
-# basically, the game requires a way to store all types for initial units and I do NOT want to write those files manually.
