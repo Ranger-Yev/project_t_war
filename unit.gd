79ca40fd36f7b222f701824732be157c1eb4d161
Yev Nosenko <yevhenii2208@gmail.com>
2026-09-22 10:43:56 -0400

    Forgot to save file :(

--- unit.gd
+++ unit.gd
@@ -26,9 +26,6 @@ var UNITCOLORS = [[Color8(50,60,210,255), Color8(15,85,125,255), Color8(130,165,
 
 
 # internal variables not meant for human eyes
-var dest = Vector2.ZERO
-var in_pos = true
-
 var BASESPEED = 200.0
 var DIVISIONS = [inf_um, inf_mntrs, inf_mil, inf_strm, inf_para, inf_sf, inf_moto, inf_mech, armor_mbt]
 
@@ -55,44 +52,8 @@ func _ready() -> void:
 	select_graphic(division_type[0], unit_color)
 
 func _process(delta: float) -> void:
-	if main_scene_node.is_selected(itself):
-		#print(itself, " is selected")
-		selected = true
-	else:
-		selected = false
-	if in_pos and selected:
-		if Input.is_action_just_pressed("RMB"):
-			dest = get_viewport().get_mouse_position()
-			print(itself, " -:- ", dest)
-			in_pos = false
-	else:
-		var name_of_parent = itself.get_parent().name
-		if name_of_parent == "federal" and selected:
-			move(dest, delta)
-			#print(dest, " : ", sprite.global_position)
-			#print("RUNNING")
-			
-		if dest == sprite.global_position:
-			in_pos = true
-		if Input.is_action_just_pressed("RMB") and selected:
-			dest = get_viewport().get_mouse_position()
-			print(itself, " -:- ", dest)
+	pass
 
-	move_and_slide()
-
-func move(pos: Vector2, delta: float):
-	#print("Division is Oscar Mike.")
-	if (position.x > dest.x - 1 and position.x < dest.x + 1) and (position.y > dest.y - 1 and position.y < dest.y + 1):
-		#print(position, " : ", dest)
-		position = dest
-		velocity = Vector2.ZERO
-	var speed_modifier = division_type[1]
-	var dir = Vector2((1.0 if pos.x > position.x else -1.0), (1.0 if pos.y > position.y else -1.0))
-	velocity = Vector2(((BASESPEED * speed_modifier) * dir.x) * delta, ((BASESPEED * speed_modifier) * dir.y) * delta)
-	if !audio_p.is_playing() and audio_play:
-		audio_p.set_playing(true)
-		audio_play = false
-		votimer.start()
 
 func select_graphic(unit_type: String, color: Color):
 	var texture = load("res://assets/unit_art/" + unit_type + ".png") as CompressedTexture2D
